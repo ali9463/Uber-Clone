52d2218db241c038f48507064aa0de35300d644c
ali9463 <chali.94063@gmail.com>
2025-09-02 10:07:03 +0500

    Captains and users done with Socket.io and Map Api

--- Frontend/src/context/SocketContext.jsx
+++ Frontend/src/context/SocketContext.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
 
-const SocketContext = () => {
-  return (
-    <div>SocketContext</div>
-  )
-}
+import React, { createContext, useEffect } from 'react';
+import { io } from 'socket.io-client';
 
-export default SocketContext
+const SocketContext = createContext();
+
+const socket = io(`${import.meta.env.VITE_BASE_URL}`); // Replace with your server URL
+
+const SocketProvider = ({ children }) => {
+    useEffect(() => {
+        // Basic connection logic
+        socket.on('connect', () => {
+          
+            console.log('Connected to server');
+        });
+
+        socket.on('disconnect', () => {
+            console.log('Disconnected from server');
+        });
+
+    }, []);
+
+
+
+    return (
+        <SocketContext.Provider value={{ socket }}>
+            {children}
+        </SocketContext.Provider>
+    );
+};
+
+export {SocketProvider, SocketContext};
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -5,16 +5,16 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import {UserContext} from './context/UserContext.jsx';
 import {CaptainContext} from './context/CapatainContext.jsx';
-import SocketProvider from './context/SocketContext.jsx';
+import {SocketProvider} from './context/SocketContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <CaptainContext>
     <UserContext>
-      {/* <SocketProvider> */}
+      <SocketProvider>
         <BrowserRouter>
           <App />
         </BrowserRouter>
-      {/* </SocketProvider> */}
+      </SocketProvider>
     </UserContext>
    </CaptainContext>
 )
